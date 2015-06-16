49f4aa21b5ca44360b2e46e169a2dc46678ed018
James Charlesworth <yorkshiretripper@gmail.com>
2015-06-16 20:17:52 +0100

    Added undefined check for session

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ module.exports = function cleanupPassportSession(req, res, next) {
 		}
 		ended = false;
 
-		if (Object.keys(req.session.passport).length === 0) {
+		if (req.session && Object.keys(req.session.passport).length === 0) {
 			delete req.session.passport;
 		}
 		_end.call(res, chunk, encoding);
